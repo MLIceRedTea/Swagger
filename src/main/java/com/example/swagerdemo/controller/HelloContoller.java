9b0695ded602777287512db36b2b0a203f4666b0
MLIceTea <2855212464@qq.com>
2020-03-02 23:44:46 +0800

    回滚提交

--- src/main/java/com/example/swagerdemo/controller/HelloContoller.java
+++ src/main/java/com/example/swagerdemo/controller/HelloContoller.java
@@ -4,7 +4,6 @@ import com.example.swagerdemo.Pojo.User;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
